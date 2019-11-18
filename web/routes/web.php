97e03782e56136a386c2ab508e637aa6cf19b867
fenibarus <hariantyfeni1202@gmail.com>
2019-11-18 12:46:32 +0700

    revisi

--- web/routes/web.php
+++ web/routes/web.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -9,6 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 Route::get('/', function () {
     return view('pages.login');
 });
@@ -19,13 +21,13 @@ Route::middleware("auth")->group(function () {
         return view('pages.dashboard');
     })->name("home");
     Route::resource('kurir','KurirController');
-<<<<<<< HEAD
+
     Route::resource('transaksi','TransaksiController');
     Route::resource('status', 'StatusController');
-=======
+
     Route::get('wilayah/list',"WilayahController@wilayahlist")->name("wilayah.list");
     Route::get('wilayah/form',"WilayahController@wilayahform")->name("wilayah.form");
->>>>>>> 6da2dcf51ef518c01de3aed2609b120531790c07
+
 });
 
 Route::get("/register",function(){
